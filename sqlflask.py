0c7247ce725040c7065c2713f63e26d89c73e502
Jan-Ru Muller <janru.muller@gmail.com>
2025-05-17 15:00:58 +0200

    Changed port number from 5000 to 8080 in sqlflask.py

--- sqlflask.py
+++ sqlflask.py
@@ -275,7 +275,7 @@ def delete_person(item_id):
     return "Invalid context", 400
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
     # Init DB if needed
     with sqlite3.connect(DATABASE) as db:
